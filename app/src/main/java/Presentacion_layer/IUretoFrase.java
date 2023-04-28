ec9f6b047fd264932740b9a9e5d9572b99996d63
Enfadaditto <nexowint02@gmail.com>
2023-04-28 17:16:19 +0200

    DescubrirFrase conectado a la base de datos

--- app/src/main/java/Presentacion_layer/IUretoFrase.java
+++ app/src/main/java/Presentacion_layer/IUretoFrase.java
@@ -162,24 +162,35 @@ public class IUretoFrase extends AppCompatActivity {
             if (i == -1) return;
             System.out.println(palabras[i] + " :: " + palabrasSolucion[i]);
             indice = escribirPalabra(palabras[i].toCharArray(), palabrasSolucion[i].toCharArray(), indice);
-            indice++;
+            ++indice;
         }
 
     }
 
     private int escribirPalabra(char[] palabraActual, char[] palabraActualSolucion, int indice) {
         System.out.println("indice inicio frase: "+ indice);
-        System.out.println("indice: " + indice);
-        //panelDescubrirFrase.getChildCount()/3
-        if (((indice % 10) + palabraActual.length) > 10 ) {
-            if (indice < 10) return escribirPalabra(palabraActual, palabraActualSolucion,10);
-            else if (indice < 20) return escribirPalabra(palabraActual, palabraActualSolucion,20);
+        System.out.println("indice: " + indice + " % " + panelDescubrirFrase.getChildCount()/3 + " = " + (indice % (panelDescubrirFrase.getChildCount()/3)));
+        System.out.println("Va a ocupar " + palabraActual.length + " paneles");
+        System.out.println((indice % (panelDescubrirFrase.getChildCount()/3)) + " + " + palabraActual.length + " = " + ((indice % (panelDescubrirFrase.getChildCount()/3)) + palabraActual.length));
+        System.out.println("¿Es esto mayor que " + (panelDescubrirFrase.getChildCount()/3) + "?");
+        if (((indice % (panelDescubrirFrase.getChildCount()/3)) + palabraActual.length) > (panelDescubrirFrase.getChildCount()/3) ) {
+            System.out.println("SI");
+            //Si el numero de la fila que este mas el largo de la palabra supera el largo de la frase:
+            if (indice < (panelDescubrirFrase.getChildCount()/3))
+                //Si la palabra se encuentra en la primera fila
+                return escribirPalabra(palabraActual, palabraActualSolucion,(panelDescubrirFrase.getChildCount()/3));
+                    //Prueba en la segunda
+            else if (indice < 2*(panelDescubrirFrase.getChildCount()/3))
+                //Si la palabra se encuentra en la segunda fila
+                return escribirPalabra(palabraActual, palabraActualSolucion,2*(panelDescubrirFrase.getChildCount()/3));
+                    //Prueba en la tercera
             else {
                 System.out.println("NO SE ESCRIBIÓ ESTA PALABRA");
                 return -1;
             }
         }
-
+        System.out.println("NO");
+        System.out.println("SE ESCRIBE PALABRA");
         for (int i = 0; i < palabraActual.length; i++) {
             Button boton = (Button) panelDescubrirFrase.getChildAt(indice + i);
             DFButton botonSolucion = new DFButton(boton, palabraActualSolucion[i]);
@@ -205,6 +216,7 @@ public class IUretoFrase extends AppCompatActivity {
                 boton.setClickable(false);
             }
         }
-        return indice += palabraActual.length;
+        indice += palabraActual.length;
+        return indice;
     }
 }

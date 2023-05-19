5a4804329e0a5ed06656ea40ba9b5e0707de0edd
RavensBrooks <raquelballesros@gmail.com>
2023-05-19 23:50:28 +0200

    armario de retos

--- app/src/main/java/Presentacion_layer/IUlogros.java
+++ app/src/main/java/Presentacion_layer/IUlogros.java
@@ -6,31 +6,66 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.slumdogsustainable.MainActivity;
 import com.slumdogsustainable.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class IUlogros extends AppCompatActivity {
+import Domain_Layer.Logro;
+import Persistence.AnswerRepository;
+import Persistence.LogroRepository;
+import Persistence.QuestionRepository;
+import Persistence.SingletonConnection;
 
-    List<TextView> enunciadosLogros = new ArrayList<>();
+public class IUlogros extends AppCompatActivity {
+    List<Logro> todosLosLogros;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logros);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        for (int i = 1; i <= 10; i++) {
-            int textViewId = getResources().getIdentifier("textoLogro" + i, "id", getPackageName());
-            TextView textView = findViewById(textViewId);
-            ponerNombresLogros(textView);
-
+        try {
+            IniciarBaseDedatos();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
 
+        ponerNombresLogros();
+        ponerDescripciones();
+
 
     }
 
-    public void ponerNombresLogros(TextView logro){
-        logro.setText("vaya vaya");
+    public void ponerNombresLogros(){
+        for (int i = 1; i <= 33; i++) {
+            int textViewId = getResources().getIdentifier("textoLogro" + i, "id", getPackageName());
+            TextView textView = findViewById(textViewId);
+            textView.setText(todosLosLogros.get(i - 1).getNombre());
+        }
     }
 
+    public void ponerDescripciones() {
+        for (int i = 1; i <= 33; i++) {
+            int textViewId = getResources().getIdentifier("descripcion" + i, "id", getPackageName());
+            TextView textView = findViewById(textViewId);
+            textView.setText(todosLosLogros.get(i - 1).getDescripcion());
+
+
+        }
+    }
+    private void IniciarBaseDedatos() throws InterruptedException {
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    todosLosLogros = new LogroRepository(SingletonConnection.getSingletonInstance()).getAllLogros();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        });
+        t.start();
+        t.join();
+    }
 }
